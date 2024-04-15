e8c73a8e100766dad834bafb0d3383ec08a717d8
Victor Benedict <victorbenedict.bulaong@gmail.com>
2024-04-16 00:43:20 +0800

    Implement dynamic content

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -1,9 +1,39 @@
-const ContactPage = () => {
-  const content = document.getElementById('content');
-  const createHeading = document.createElement('h1');
+import data from '../assets/content.json';
 
-  createHeading.textContent = 'Contact Us';
-  content.appendChild(createHeading);
+const ContactPage = () => {
+  const { header, description, email, phone, address } = data.contact;
+  const content = document.getElementById('content');
+
+  const createHeader = document.createElement('h1');
+  createHeader.textContent = header;
+  content.appendChild(createHeader);
+
+  const descriptionParagraph = document.createElement('p');
+  descriptionParagraph.textContent = description;
+  content.appendChild(descriptionParagraph);
+
+  const contactContainer = document.createElement('div');
+  content.appendChild(contactContainer);
+
+  const emailParagraph = document.createElement('p');
+  emailParagraph.textContent = 'Email: ';
+  const emailLink = document.createElement('a');
+  emailLink.textContent = email;
+  emailLink.href = `mailto${email}`;
+  emailParagraph.appendChild(emailLink);
+  contactContainer.appendChild(emailParagraph);
+
+  const phoneParagraph = document.createElement('p');
+  phoneParagraph.textContent = 'Phone: ';
+  const phoneLink = document.createElement('a');
+  phoneLink.textContent = phone;
+  phoneLink.href = `tel:${phone}`;
+  phoneParagraph.appendChild(phoneLink);
+  contactContainer.appendChild(phoneParagraph);
+
+  const addressParagraph = document.createElement('p');
+  addressParagraph.textContent = `Address: ${address}`;
+  contactContainer.appendChild(addressParagraph);
 };
 
 export default ContactPage;
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,22 +1,23 @@
 import MenuPage from './MenuPage';
 import ContactPage from './ContactPage';
 import NavButton from '../components/NavButton';
+import data from '../assets/content.json';
 
 const HomePage = () => {
+  const { header, description } = data.home;
   const content = document.getElementById('content');
+
   const createHeading = document.createElement('h1');
+  createHeading.textContent = header;
+  content.appendChild(createHeading);
+
   const paragraph = document.createElement('p');
+  paragraph.textContent = description;
+  content.appendChild(paragraph);
+
   const buttonContainer = document.createElement('div');
-
-  createHeading.textContent = 'Welcome to SisBrew!';
-  paragraph.textContent =
-    'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Inventore, dolorem veniam dolorum ex, ipsa explicabo magnam accusamus aspernatur repellendus ut vero ducimus. Magni, illum qui incidunt error libero ex cupiditate!';
-
   buttonContainer.appendChild(NavButton('Our Menu', MenuPage));
   buttonContainer.appendChild(NavButton('Contact Us', ContactPage));
-
-  content.appendChild(createHeading);
-  content.appendChild(paragraph);
   content.appendChild(buttonContainer);
 };
 
